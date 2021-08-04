d0a8b4b3775e4568b3ff570a8e713bb1de959512
Rudzky <lubieprogramowanie@gmail.com>
2021-08-04 13:06:47 +0200

    Add simple signup form

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,39 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import "./App.css";
 // import Users from "./components/Users";
 import Landing from "./pages/Landing";
+import { setContext } from "apollo-link-context";
+import Signup from "./pages/Signup";
 
+const httpLink = new HttpLink({ uri: "http://localhost:4000" });
+const authLink = setContext((req, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    ...headers,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const link = authLink.concat(httpLink as any);
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  link: link as any,
   cache: new InMemoryCache(),
 });
+
+// const client = new ApolloClient({
+//   uri: "http://localhost:4000",
+//   cache: new InMemoryCache(),
+// });
 
 function App() {
   return (
@@ -21,6 +46,9 @@ function App() {
           </Route>
           <Route path="/landing">
             <Landing />
+          </Route>
+          <Route path="/signup">
+            <Signup />
           </Route>
         </Switch>
       </Router>
